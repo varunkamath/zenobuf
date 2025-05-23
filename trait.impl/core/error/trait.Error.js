315c822b7a192cb0ff7c71b32efb8a1cbb33c24f
varunkamath <varun.kamath@gmail.com>
2025-05-23 07:10:32 +0000

    Deploying to gh-pages from @ varunkamath/zenobuf@1095c439c46c4db71fa853264452f2978d5b098a 🚀

--- trait.impl/core/error/trait.Error.js
+++ trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["zenobuf_cli",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"zenobuf_cli/error/enum.Error.html\" title=\"enum zenobuf_cli::error::Error\">Error</a>"]]],["zenobuf_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"zenobuf_core/error/enum.Error.html\" title=\"enum zenobuf_core::error::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["zenobuf_cli",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"zenobuf_cli/error/enum.Error.html\" title=\"enum zenobuf_cli::error::Error\">Error</a>"]]],["zenobuf_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"zenobuf_core/error/enum.Error.html\" title=\"enum zenobuf_core::error::Error\">Error</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["zenobuf_cli",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"zenobuf_cli/error/enum.Error.html\" title=\"enum zenobuf_cli::error::Error\">Error</a>"]]],["zenobuf_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"zenobuf_core/error/enum.Error.html\" title=\"enum zenobuf_core::error::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["zenobuf_cli",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"zenobuf_cli/error/enum.Error.html\" title=\"enum zenobuf_cli::error::Error\">Error</a>"]]],["zenobuf_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"zenobuf_core/error/enum.Error.html\" title=\"enum zenobuf_core::error::Error\">Error</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
