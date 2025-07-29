59a98bdc705a3c78673b5cba702722a3b54fa256
varunkamath <varun.kamath@gmail.com>
2025-07-29 02:39:13 +0000

    Deploying to gh-pages from @ varunkamath/zenobuf@ba14e91498ec03bf90512855ec17e29c307263bf 🚀

---
+++ trait.impl/core/hash/trait.Hash.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["zenobuf_examples",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"zenobuf_examples/proto/service/struct.AddTwoIntsRequest.html\" title=\"struct zenobuf_examples::proto::service::AddTwoIntsRequest\">AddTwoIntsRequest</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"zenobuf_examples/proto/service/struct.AddTwoIntsResponse.html\" title=\"struct zenobuf_examples::proto::service::AddTwoIntsResponse\">AddTwoIntsResponse</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[671]}
