a7626eddde331979c85a0031b974d22935b05b4f
varunkamath <varun.kamath@gmail.com>
2025-07-29 02:33:50 +0000

    Deploying to gh-pages from @ varunkamath/zenobuf@a5647eb4e6c0c148a5fbc03855d2e1f7860fe129 🚀

--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["zenobuf_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"zenobuf_core/node/struct.DropGuard.html\" title=\"struct zenobuf_core::node::DropGuard\">DropGuard</a>"]]]]);
+    var implementors = Object.fromEntries([["zenobuf_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"zenobuf_core/node/struct.DropGuard.html\" title=\"struct zenobuf_core::node::DropGuard\">DropGuard</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
